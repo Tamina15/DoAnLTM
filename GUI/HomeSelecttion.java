15ddfd67ae2331e58600774c06acc1d036c31e2c
nhichap <72775391+nhichap@users.noreply.github.com>
2022-10-31 12:42:24 +0700

    làm lại gui

---
+++ GUI/HomeSelecttion.java
@@ -0,0 +1,113 @@
+package GUI;
+
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.JEditorPane;
+import javax.swing.JSplitPane;
+import javax.swing.JDesktopPane;
+
+public class HomeSelecttion extends JFrame {
+	
+	private JPanel contentPane;
+	private JLabel lbl_ImageBackground, lbl_IconInfor, lbl_IconGame, lbl_IconEdit, lbl_IconTop;
+	private JButton btnRegister, btnQuit;
+	int widthLeft;
+	private JPanel pnBtn1,pnBtn2, pnBtn3;
+	private JLabel lblNewLabel;
+	private JLabel lblNewLabel_1;
+	
+	public HomeSelecttion() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(50, 100, 633, 350);
+		setUndecorated(true);
+		setLocationRelativeTo(null);
+		initComponens(); // initComponens
+//		addEvents();
+	}
+
+	public void initComponens() {
+		contentPane = new JPanel();
+		contentPane.setLayout(null);
+		setContentPane(contentPane);
+		
+		//icon vào game
+		lbl_IconInfor = new JLabel("");
+		lbl_IconInfor.setBackground(new Color(255, 255, 255));
+		lbl_IconInfor.setHorizontalAlignment(SwingConstants.LEFT);
+		lbl_IconInfor.setIcon(new ImageIcon(LoginForm.class.getResource("/images/iconplay.png")));
+		lbl_IconInfor.setBounds(212, 104, 250, 125);
+		contentPane.add(lbl_IconInfor);
+		
+		//icon đăng xuất
+		
+		lbl_IconGame = new JLabel("");
+		lbl_IconGame.setBackground(new Color(255, 255, 255));
+		lbl_IconGame.setHorizontalAlignment(SwingConstants.LEFT);
+		lbl_IconGame.setIcon(new ImageIcon(LoginForm.class.getResource("/images/logout.png")));
+		lbl_IconGame.setBounds(594, 0, 29, 43);
+		contentPane.add(lbl_IconGame);
+		
+		//icon chỉnh sửa thông tin cá nhân
+		
+		lbl_IconEdit = new JLabel("");
+		lbl_IconEdit.setBackground(new Color(255, 255, 255));
+		lbl_IconEdit.setHorizontalAlignment(SwingConstants.LEFT);
+		lbl_IconEdit.setIcon(new ImageIcon(LoginForm.class.getResource("/images/edit.png")));
+		lbl_IconEdit.setBounds(20, 233, 37, 43);
+		contentPane.add(lbl_IconEdit);
+		
+		//icon xếp hạng
+		
+		lbl_IconTop = new JLabel("");
+		lbl_IconTop.setBounds(20, 287, 49, 37);
+		lbl_IconTop.setBackground(new Color(255, 255, 255));
+		lbl_IconTop.setHorizontalAlignment(SwingConstants.LEFT);
+		lbl_IconTop.setIcon(new ImageIcon(LoginForm.class.getResource("/images/analysis.png")));
+		contentPane.add(lbl_IconTop);
+		
+		
+		//hình nền game
+		lbl_ImageBackground = new JLabel("");
+		lbl_ImageBackground.setHorizontalAlignment(SwingConstants.LEFT);
+		lbl_ImageBackground.setIcon(new ImageIcon(LoginForm.class.getResource("/images/background_home.png")));
+		lbl_ImageBackground.setBounds(0, 0, 633, 350);
+		contentPane.add(lbl_ImageBackground);
+		
+
+
+		  
+		
+	}
+	
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					HomeSelecttion frame = new HomeSelecttion();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
--- GUI/LoginForm.java
+++ GUI/LoginForm.java
@@ -23,7 +23,6 @@ import javax.swing.JTextField;
 
 import javax.swing.border.MatteBorder;
 
-
 import javax.swing.JPasswordField;
 
 public class LoginForm extends JFrame {
@@ -31,14 +30,14 @@ public class LoginForm extends JFrame {
 	int widthLeft;
 	private JPanel contentPane;
 	private JPanel pnBtnBar;
-	private JLabel lblClose, lblMinimize;
+	private JLabel lblClose, lblMinimize, lbl_IconBack;
 	JPanel pnHome;
 	private JPanel pnInput;
-	private JLabel lblNewLabel;
+	private JLabel lblNewLabel, lblMtKhu;
 	private JPasswordField txt_pass;
 	private JPanel pnBtn;
 	private JLabel lbl_IconDeliveryman;
-	private JButton btnLogin;
+	private JButton btnLogin, btnBack;
 	private JTextField txt_username;
 
 	public LoginForm() {
@@ -47,182 +46,146 @@ public class LoginForm extends JFrame {
 		setUndecorated(true);
 		setLocationRelativeTo(null);
 		initComponens(); // initComponens
-		addEvents();
+//		addEvents();
 	}
 
 	public void initComponens() {
 		contentPane = new JPanel();
 		contentPane.setLayout(null);
 		setContentPane(contentPane);
-
-		pnBtnBar = new JPanel();
-		pnBtnBar.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
-		pnBtnBar.setBackground(new Color(0, 128, 128));
-		pnBtnBar.setBounds(308, 0, 326, 30);
-		contentPane.add(pnBtnBar);
-		pnBtnBar.setLayout(null);
-
-		lblMinimize = new JLabel("-");
-
-		lblMinimize.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		lblMinimize.setForeground(Color.WHITE);
-		lblMinimize.setHorizontalAlignment(SwingConstants.CENTER);
-		lblMinimize.setFont(new Font("Tahoma", Font.PLAIN, 21));
-		lblMinimize.setBounds(256, 0, 30, 30);
-		pnBtnBar.add(lblMinimize);
-
-		lblClose = new JLabel("x");
-		lblClose.setBackground(new Color(0, 128, 128));
-		lblClose.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		lblClose.setForeground(Color.WHITE);
-		lblClose.setBounds(296, 0, 30, 30);
-		pnBtnBar.add(lblClose);
-		lblClose.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblClose.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		lbl_IconBack = new JLabel("");
+		lbl_IconBack.setBounds(10, 10, 40, 30);
+		lbl_IconBack.setBackground(new Color(255, 255, 255));
+		lbl_IconBack.setHorizontalAlignment(SwingConstants.LEFT);
+		lbl_IconBack.setIcon(new ImageIcon(LoginForm.class.getResource("/images/iconback.png")));
+		contentPane.add(lbl_IconBack);
+		
+		lblMtKhu = new JLabel("M\u1EADt kh\u1EA9u");
+		lblMtKhu.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		lblMtKhu.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		lblMtKhu.setForeground(Color.WHITE);
+		lblMtKhu.setFont(new Font("Segoe UI", Font.PLAIN, 17));
+		lblMtKhu.setBounds(210, 107, 206, 25);
+		contentPane.add(lblMtKhu);
+		
+		lblNewLabel = new JLabel("Email");
+		lblNewLabel.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		lblNewLabel.setForeground(Color.WHITE);
+		lblNewLabel.setFont(new Font("Segoe UI", Font.PLAIN, 17));
+		lblNewLabel.setBounds(210, 38, 100, 25);
+		contentPane.add(lblNewLabel);
+		
+		lbl_IconDeliveryman = new JLabel("");
+		lbl_IconDeliveryman.setHorizontalAlignment(SwingConstants.LEFT);
+		lbl_IconDeliveryman.setIcon(new ImageIcon(LoginForm.class.getResource("/images/background_home.png")));
+		lbl_IconDeliveryman.setBounds(0, 0, 633, 350);
+		contentPane.add(lbl_IconDeliveryman);
+		widthLeft = (int) contentPane.getPreferredSize().getWidth();
 
 		JPanel pnContent = new JPanel();
 		pnContent.setFont(new Font("Segoe UI", Font.PLAIN, 15));
 		pnContent.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		pnContent.setBackground(new Color(0, 139, 139));
-		pnContent.setBounds(308, 29, 326, 321);
+		pnContent.setBackground(new Color(255, 255, 255));
+		pnContent.setBounds(0, 0, 634, 350);//308, 0, 326, 30 0, 0, 308, 350
 		contentPane.add(pnContent);
 		pnContent.setLayout(null);
 
-		pnInput = new JPanel();
-		pnInput.setOpaque(false);
-		pnInput.setBounds(0, 0, 326, 224);
-		pnContent.add(pnInput);
-		pnInput.setLayout(null);
-
-		lblNewLabel = new JLabel("T\u00E0i kho\u1EA3n");
-		lblNewLabel.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
-		lblNewLabel.setForeground(Color.WHITE);
-		lblNewLabel.setFont(new Font("Segoe UI", Font.PLAIN, 17));
-		lblNewLabel.setBounds(42, 38, 100, 25);
-		pnInput.add(lblNewLabel);
-
 		txt_username = new JTextField();
+		txt_username.setOpaque(false);
 		txt_username.setFont(new Font("Segoe UI", Font.PLAIN, 15));
-		txt_username.setBorder(new MatteBorder(0, 0, 2, 0, (Color) new Color(255, 255, 255)));
-		txt_username.setHorizontalAlignment(SwingConstants.LEFT);
-		txt_username.setCaretColor(new Color(255, 255, 255));
-		txt_username.setBackground(new Color(0, 139, 139));
 		txt_username.setDisabledTextColor(new Color(0, 139, 139));
-		txt_username.setForeground(new Color(255, 255, 255));
-		txt_username.setSelectedTextColor(new Color(0, 139, 139));
-		txt_username.setBounds(42, 63, 206, 25);
-		pnInput.add(txt_username);
+		txt_username.setCaretColor(new Color(255, 255, 255));
+		txt_username.setBounds(210, 63, 206, 30);
+		
+		pnContent.add(txt_username);
 		txt_username.setColumns(10);
 
-		JLabel lblMtKhu = new JLabel("M\u1EADt kh\u1EA9u");
-		lblMtKhu.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
-		lblMtKhu.setForeground(Color.WHITE);
-		lblMtKhu.setFont(new Font("Segoe UI", Font.PLAIN, 17));
-		lblMtKhu.setBounds(42, 107, 100, 25);
-		pnInput.add(lblMtKhu);
-
 		txt_pass = new JPasswordField();
-		txt_pass.setForeground(new Color(255, 255, 255));
-		txt_pass.setDisabledTextColor(new Color(255, 255, 255));
 		txt_pass.setOpaque(false);
 		txt_pass.setBorder(new MatteBorder(0, 0, 2, 0, (Color) new Color(255, 255, 255)));
 		txt_pass.setCaretColor(new Color(255, 255, 255));
-		txt_pass.setBounds(42, 132, 206, 25);
-		pnInput.add(txt_pass);
+		txt_pass.setBounds(210, 132, 206, 30);
+		pnContent.add(txt_pass);                                 
 
-		pnBtn = new JPanel();
-		pnBtn.setOpaque(false);
-		pnBtn.setBounds(39, 245, 234, 30);
-		pnContent.add(pnBtn);
-		pnBtn.setLayout(null);
-
-		btnLogin = new JButton("\u0110\u0103ng nh\u1EADp");
-		btnLogin.setForeground(new Color(0, 128, 0));
+		btnLogin = new JButton("Đăng nhập");
+		btnLogin.setForeground(new Color(255, 255, 255));
 
 		btnLogin.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		btnLogin.setBackground(new Color(255, 255, 255));
+		btnLogin.setBackground(new Color(51, 51, 102));
 		btnLogin.setFont(new Font("Segoe UI", Font.BOLD, 14));
-		btnLogin.setBounds(0, 0, 234, 30);
-		pnBtn.add(btnLogin);
+		btnLogin.setBounds(210, 192, 206, 40);
+		pnContent.add(btnLogin);
+		
 
-		JPanel pnLogo = new JPanel();
-		pnLogo.setForeground(new Color(255, 255, 255));
-		pnLogo.setBorder(new MatteBorder(0, 0, 0, 3, (Color) new Color(255, 255, 255)));
-		pnLogo.setBackground(new Color(70, 139, 139));
-		pnLogo.setBounds(0, 0, 308, 350);
-		contentPane.add(pnLogo);
-		pnLogo.setLayout(null);
+		
 
-		lbl_IconDeliveryman = new JLabel("");
-		lbl_IconDeliveryman.setHorizontalAlignment(SwingConstants.LEFT);
-		lbl_IconDeliveryman.setIcon(new ImageIcon(LoginForm.class.getResource("/images/logo.png")));
-		lbl_IconDeliveryman.setBounds(-52, 11, 346, 328);
-		pnLogo.add(lbl_IconDeliveryman);
-		widthLeft = (int) pnLogo.getPreferredSize().getWidth();
+
+		
 
 	}
 
 	// Event
-	public void addEvents() {
-		lblClose.addMouseListener(new MouseAdapter() {
-
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				System.exit(0);
-			}
-
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				lblClose.setBackground(new Color(47, 79, 79));
-				lblClose.setOpaque(true);
-			}
-
-			@Override
-			public void mouseExited(MouseEvent e) {
-//				lblClose.setOpaque(false);
-				lblClose.setBackground(new Color(0, 128, 128));
-			}
-		});
-
-		lblMinimize.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				setState(Frame.ICONIFIED);
-			}
-
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				lblMinimize.setBackground(new Color(47, 79, 79));
-				lblMinimize.setOpaque(true);
-			}
-
-			@Override
-			public void mouseExited(MouseEvent e) {
-//				lblMini.setOpaque(false);
-				lblMinimize.setBackground(new Color(0, 128, 128));
-			}
-		});
-
-		pnBtnBar.addMouseMotionListener(new MouseMotionAdapter() {
-			@Override
-			public void mouseDragged(MouseEvent e) {
-				int positionX = e.getXOnScreen();
-				int positionY = e.getYOnScreen();
-				moveWindow(positionX - posX - widthLeft - 100, positionY - posY);
-			}
-		});
-
-		// Login
-		
-		btnLogin.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				String username = txt_username.getText();
-				String password = String.valueOf(txt_pass.getPassword());
-				loadHomeForm();
-			}
-		});
-	}
+//	public void addEvents() {
+//		lblClose.addMouseListener(new MouseAdapter() {
+//
+//			@Override
+//			public void mouseClicked(MouseEvent e) {
+//				System.exit(0);
+//			}
+//
+//			@Override
+//			public void mouseEntered(MouseEvent e) {
+//				lblClose.setBackground(new Color(47, 79, 79));
+//				lblClose.setOpaque(true);
+//			}
+//
+//			@Override
+//			public void mouseExited(MouseEvent e) {
+////				lblClose.setOpaque(false);
+//				lblClose.setBackground(new Color(0, 128, 128));
+//			}
+//		});
+//
+//		lblMinimize.addMouseListener(new MouseAdapter() {
+//			@Override
+//			public void mouseClicked(MouseEvent e) {
+//				setState(Frame.ICONIFIED);
+//			}
+//
+//			@Override
+//			public void mouseEntered(MouseEvent e) {
+//				lblMinimize.setBackground(new Color(47, 79, 79));
+//				lblMinimize.setOpaque(true);
+//			}
+//
+//			@Override
+//			public void mouseExited(MouseEvent e) {
+////				lblMini.setOpaque(false);
+//				lblMinimize.setBackground(new Color(0, 128, 128));
+//			}
+//		});
+//
+//		pnBtnBar.addMouseMotionListener(new MouseMotionAdapter() {
+//			@Override
+//			public void mouseDragged(MouseEvent e) {
+//				int positionX = e.getXOnScreen();
+//				int positionY = e.getYOnScreen();
+//				moveWindow(positionX - posX - widthLeft - 100, positionY - posY);
+//			}
+//		});
+//
+//		// Login
+//		
+//		btnLogin.addMouseListener(new MouseAdapter() {
+//			@Override
+//			public void mouseClicked(MouseEvent e) {
+//				String username = txt_username.getText();
+//				String password = String.valueOf(txt_pass.getPassword());
+//				loadHomeForm();
+//			}
+//		});
+//	}
 
 	public void moveWindow(int positionX, int positionY) {
 		this.setLocation(positionX, positionY);
@@ -232,15 +195,10 @@ public class LoginForm extends JFrame {
 		this.dispose();
 	}
 	
+	
 	 public void loadHomeForm() {
-		 HomeForm homeForm = new HomeForm();
+		 HomeSelecttion homeForm = new HomeSelecttion();
 		 homeForm.setVisible(true);
 		 closeThisWindow();
 	 }
-	// public
-
-	public static void main(String[] args) {
-		LoginForm login = new LoginForm();
-		login.setVisible(true);
-	}
 }
