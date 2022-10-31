60dbcc75b3f486f6a2dc575670f0e452a840886f
Tamina15 <minhnhat66442658@gmail.com>
2022-10-31 20:42:26 +0700

    add constant class

---
+++ src/Utils/Constant.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utils;
+
+/**
+ *
+ * @author Tran Minh Nhat
+ */
+public class Constant {
+
+    public static final int ORIGINAL_TILE_SIZE = 16;
+    public static final int SCALE = 4;
+    public static final int TILE_SIZE = ORIGINAL_TILE_SIZE * SCALE;
+    public static final int COLUMN = 20;
+    public static final int ROW = 10;
+    public static final int PANEL_WIDTH = TILE_SIZE * COLUMN;
+    public static final int PANEL_HEIGHT = TILE_SIZE * ROW;
+    public static int NUMBER_AMOUNT = 100;
+}
