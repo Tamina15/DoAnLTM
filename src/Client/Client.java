10f12d0a133ec9ac0b202e4eca74734cd08770d3
Tamina15 <minhnhat66442658@gmail.com>
2022-11-05 17:14:48 +0700

    add client, server

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -4,10 +4,146 @@
  * and open the template in the editor.
  */
 package Client;
+
+import static Utils.Constant.PORT;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Tran Minh Nhat
  */
 public class Client {
-    
+
+    InetAddress address;
+    DatagramSocket socket;
+    DatagramPacket receivePacket, sendPacket;
+    Scanner scanner;
+    Thread s, r;
+    boolean flag = true;
+
+    Client() {
+        try {
+            address = InetAddress.getByName("localhost");
+            socket = new DatagramSocket();
+            scanner = new Scanner(System.in);
+            s = new Thread(new Send());
+            r = new Thread(new Receive());
+        } catch (SocketException | UnknownHostException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void StartThread() {
+        s.start();
+        r.start();
+    }
+
+    public void SendData() {
+        byte[] data;
+        while (true) {
+            try {
+                System.out.print("User input: ");
+                String string = scanner.nextLine();
+
+                data = string.getBytes();
+                sendPacket = new DatagramPacket(data = new byte[data.length], data.length, address, PORT);
+                // Send Packet to Server
+
+                socket.send(sendPacket);
+
+                if (string.equals("bye") || socket.isConnected()) {
+
+                    break;
+                }
+
+                // Receive Packet From Server
+                receivePacket = new DatagramPacket(data = new byte[data.length], data.length);
+                socket.receive(receivePacket);
+                string = new String(receivePacket.getData());
+                System.out.println(string);
+            } catch (IOException ex) {
+                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    public void CloseClient() {
+        System.out.println("Client Disconnect");
+        scanner.close();
+        socket.close();
+    }
+
+    class Send implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println("Client ready to send data");
+            while (flag) {
+                try {
+                    byte[] send = new byte[150];
+                    System.out.print("User input: ");
+                    String string = scanner.nextLine();
+                    send = string.getBytes();
+                    System.out.println(string);
+                    sendPacket = new DatagramPacket(send, send.length, address, PORT);
+                    // Send Packet to Server
+                    socket.send(sendPacket);
+                    if (string.equals("bye") || socket.isConnected()) {
+                        flag = false;
+                        break;
+                    }
+                } catch (IOException ex) {
+                    flag = false;
+                }
+            }
+
+        }
+
+    }
+
+    class Receive implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println("Client is Receiving data");
+            while (flag) {
+                try {
+                    byte[] receive = new byte[150];
+                    receivePacket = new DatagramPacket(receive, receive.length);
+                    socket.receive(receivePacket);
+                    String string = new String(receivePacket.getData());
+                    DoSomething(string);
+                } catch (IOException ex) {
+                    flag = false;
+                }
+
+            }
+        }
+
+        private void DoSomething(String s) {
+            System.out.println(s);
+        }
+    }
+
+}
+
+class main {
+
+    public static void main(String[] args) throws InterruptedException {
+        Client client = new Client();
+        //client.SendData();
+        client.StartThread();
+        client.r.join();
+        client.s.join();
+        client.CloseClient();
+        System.out.println("Client complete");
+    }
 }
--- src/Server/Server.java
+++ src/Server/Server.java
@@ -6,7 +6,14 @@
 package Server;
 
 import static Utils.Constant.NUMBER_AMOUNT;
+import static Utils.Constant.PORT;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -14,10 +21,124 @@ import java.util.Random;
  */
 public class Server {
 
+    DatagramSocket socket;
+    DatagramPacket receivePacket, sendPacket;
+    byte[] buffer = new byte[1024];
+    int port = 3000;
+    Thread r, s;
+    boolean flag = true;
+    boolean doSend = false;
+
     public static int currentNumber;
     private static int currentIndex = 0;
-
     public static int[] array = new int[NUMBER_AMOUNT];
+
+    Server() {
+        try {
+            socket = new DatagramSocket(PORT);
+            System.out.println("Start Server at " + socket.getLocalPort());
+        } catch (SocketException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void Receive() {
+        while (true) {
+            try {
+                // Receive data from client
+                buffer = new byte[1024];
+                receivePacket = new DatagramPacket(buffer, buffer.length);
+                socket.receive(receivePacket);
+                int z = 0;
+                for (int i = 0; i < buffer.length; i++) {
+                    if (buffer[i] == 0) {
+                        z = i;
+                        break;
+                    }
+                }
+                String string = new String(receivePacket.getData(), 0, z);
+                System.out.println("Server received: " + string + " from " + receivePacket.getAddress().getHostName());
+                if (string.equals("bye")) {
+                    sendPacket = new DatagramPacket(new byte[0], 0, receivePacket.getAddress(), receivePacket.getPort());
+                    socket.send(sendPacket);
+                    break;
+                }
+                // Send data to client
+                String result = Process(string);
+                System.out.println(result);
+
+                sendPacket = new DatagramPacket(result.getBytes(), result.getBytes().length, receivePacket.getAddress(), receivePacket.getPort());
+                socket.send(sendPacket);
+
+            } catch (IOException ex) {
+                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+                break;
+            }
+        }
+
+    }
+
+    public String Process(String word) throws IOException {
+        String result = "Sai cu phap";
+        return result;
+    }
+
+    public void CloseServer() {
+        socket.close();
+    }
+
+    class Send implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println("Server ready to send data");
+            while (flag) {
+                if (doSend) {
+                    try {
+                        String s = DoSomething();
+                        sendPacket = new DatagramPacket(new byte[150], 150, receivePacket.getAddress(), PORT);
+                        // Send Packet to Server
+                        socket.send(sendPacket);
+                        if (string.equals("bye") || socket.isConnected()) {
+                            flag = false;
+                            break;
+                        }
+                    } catch (IOException ex) {
+                        flag = false;
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    class Receive implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println("Server is Receiving data");
+            while (flag) {
+                try {
+                    byte[] receive = new byte[150];
+                    receivePacket = new DatagramPacket(receive, receive.length);
+                    socket.receive(receivePacket);
+                    String string = new String(receivePacket.getData());
+                    DoSomething(string);
+                    doSend = true;
+                } catch (IOException ex) {
+                    flag = false;
+                }
+
+            }
+        }
+
+        private String DoSomething(String string) {
+            System.out.println(string);
+            return string;
+        }
+
+    }
 
     public static void RandomizeArray() {
         for (int i = 0; i < array.length; i++) {
@@ -38,5 +159,16 @@ public class Server {
         if (currentIndex + 1 <= NUMBER_AMOUNT) {
             currentNumber = array[currentIndex++];
         }
+    }
+
+}
+
+class main {
+
+    public static void main(String[] args) throws IOException {
+        Server server = new Server();
+        server.Receive();
+        server.CloseServer();
+
     }
 }
